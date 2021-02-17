c52a7193e5ace3cd5eaa138e204dfc0bc965a3f1
Daniel Marin <danielarturomt@gmail.com>
2021-02-17 17:31:10 -0300

    feat: definicion de tipos Oraculo (#7) closes #2

---
+++ Oraculo.hs
@@ -0,0 +1,33 @@
+module Oraculo where
+import Data.Map
+
+data Oraculo = Pregunta String Opciones | Prediccion String 
+                deriving(Show,Read)
+
+type Opciones = Map String Oraculo
+
+crearOraculo :: String -> Oraculo
+crearOraculo s = Prediccion s
+
+prediccion :: Oraculo -> String
+prediccion (Prediccion s) = s
+prediccion _ = error "Predicción inválida."
+
+pregunta :: Oraculo -> String
+pregunta (Pregunta s _) = s
+pregunta _ = error "Pregunta inválida."
+
+opciones :: Oraculo -> Opciones
+opciones (Pregunta _ opciones) = opciones
+opciones _ = error "Pregunta inválida."
+
+respuesta :: Oraculo -> String -> Oraculo
+respuesta (Pregunta _ opciones) clave =
+    case (Map.lookup clave opciones) of
+        Just opcion -> opcion
+        Nothing -> error "Respuesta inválida."
+respuesta _ _ = error "Pregunta inválida."
+
+ramificar :: [String] -> [Oraculo] -> String -> Oraculo
+ramificar opciones oraculos pregunta =
+    Pregunta pregunta (fromList (zip opciones oraculos))
