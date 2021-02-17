dd417b417c2f0533f27c9f929575611d590ffe72
Daniel Marin <danielarturomt@gmail.com>
2021-02-17 17:28:12 -0300

    feat: crear un menu sencillo (#6) closes #1

--- Haskinator.hs
+++ Haskinator.hs
@@ -2,9 +2,46 @@ module Haskinator(main) where
 import System.Exit
 import System.IO
 
-menu :: Maybe a -> IO()
-menu _ = do
-        putStrLn "Introduzca una opción."
+menu :: IO()
+menu = do
+  putStrLn "Introduzca una opción."
+  putStrLn "1) Crear un Oráculo nuevo."
+  putStrLn "2) Predecir."
+  putStrLn "3) Persistir."
+  putStrLn "4) Cargar."
+  putStrLn "5) Consultar pregunta crucial."
+  putStrLn "6) Salir."
+  opcion <- getLine
+  oraculoNuevo <- case opcion of
+    "1" -> crearNuevoOraculo
+    "2" -> predecir
+    "3" -> persistir
+    "4" -> cargar
+    "5" -> consultarPreguntaCritica
+    "6" -> exitSuccess
+    _ -> do
+      putStrLn "Opción incorrecta."
+  menu
+
+crearNuevoOraculo :: IO()
+crearNuevoOraculo = do
+  putStrLn "Introduzca una predicción."
+
+predecir :: IO()
+predecir = do
+  putStrLn "Predecir"
+
+persistir :: IO()
+persistir = do
+  putStrLn "Introduzca el archivo para guardar el oráculo."
+
+cargar :: IO()
+cargar = do
+  putStrLn "Introduzca el archivo a leer."
+
+consultarPreguntaCritica :: IO()
+consultarPreguntaCritica = do
+  putStrLn "Consultar pregunta crítica."
 
 main::IO()
-main = menu Nothing
+main = menu
